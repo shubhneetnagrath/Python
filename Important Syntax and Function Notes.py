570c50d67db9c90673d695a911c44ba1a660a245
Shubhneet Nagrath <shubhneetnagrath@gmail.com>
2026-06-21 11:00:25 +0530

    Syntax Notes

---
+++ Important Syntax and Function Notes.py
@@ -0,0 +1,37 @@
+'''
+>>>INDENTATIONS
+
+   >Indentation refers to the spaces at the beginning of a code line
+
+   >Where in other programming languages the indentation in code is for readability
+    only, the indentation in Python is very important.
+
+   >Python uses indentation to indicate a block of code.
+
+
+>>>STATEMENTS
+
+   >A computer program is a list of "instructions" to be "executed" by a computer.
+   In a programming language, these programming instructions are called statements.
+
+
+>>>DIFFERENT TYPES OF CASES IN VARIABLE
+
+   >Camel Case
+     Each word, except the first, starts with a capital letter:
+       
+         myVariableName = "John"
+
+   >Pascal Case
+     Each word starts with a capital letter:
+
+         MyVariableName = "John"
+
+   >Snake Case
+     Each word is separated by an underscore character:
+
+         my_variable_name = "John"
+                  
+      
+
+'''
